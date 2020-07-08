6565c0e0a3bc0def56c2af02ff3b38986c47656e
mkadiayi <michaelkadiayi@gmail.com>
2020-07-07 19:19:21 -0500

    stamdar app is working

---
+++ public/js/chat.js
@@ -0,0 +1,53 @@
+const socket = io()
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageButton = $messageForm.querySelector('button')
+const $sendLocation = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {message})
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationMessageTemplate, {url})
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error) => {
+        $messageButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
+})
+$sendLocation.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    $sendLocation.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocation.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
+
+})
+
